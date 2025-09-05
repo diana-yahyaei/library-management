d60a3b7840ff1c3d08456ceaae9c75d1b6f9b645
diana <@dianayahyaei2005@gmail.com>
2025-09-06 00:18:06 +0330

    conditions

--- library.js
+++ library.js
@@ -7,17 +7,52 @@ let library = [
 
 let user = {
   name: "Alice",
-  borrowedBooks: [],
+  borrowedBooks: [1],
   borrowBook: function (bookTitle) {
     // implement borrowing logic here
+    if (library.includes(library[0]) === true) {
+      console.log(
+        "user" +
+          object.name +
+          " borrowed" +
+          library[0] +
+          "total borrowed:" +
+          this.borrowBook
+      );
+    } else if (library.includes(library[0]) === false) {
+      console.log(`sorry ${library[0]} is not available`);
+    } else {
+      console.log(`${library[0]} not founded`);
+    }
   },
   returnBook: function (bookTitle) {
     // implement returning logic here
+    if (user.borrowBook === true) {
+      user.borrowBook = 0;
+      console.log("user" + user.name + "returned" + library[0]);
+    } else {
+      console.log(`user has not borrowed ${library[0]}`);
+    }
   },
 };
+// let user2 = {
+//   name: "hanna",
+//   borrowedBooks: [],
+//   borrowBook: function (bookTitle) {
+//     // implement borrowing logic here
+//   },
+//   returnBook: function (bookTitle) {
+//     // implement returning logic here
+//   },
+// };
 for (let i = 0; i < library.length; i++) {
   console.log(library[i]);
 }
 
 const name = prompt("enter book`s name you want:");
-// console.log(library.includes(library.title));
+console.log(library.includes(library[0]));
+console.log(borrowBook);
+console.log(user.borrowBook);
+while (user.borrowBook === true) {
+  borrowBook++;
+}
